26d3d1e83213177483e2ba88bfe46a65176724e9
hutamatr <hutamatr@gmail.com>
2023-01-28 19:29:47 +0700

    refactoring category form

--- src/components/Category/CategoryForm.jsx
+++ src/components/Category/CategoryForm.jsx
@@ -1,119 +1,55 @@
-import { useState } from 'react';
-import { useQueryClient, useMutation } from '@tanstack/react-query';
-import { toast } from 'react-hot-toast';
-
-import Modal from 'components/UI/Modal';
-import useHttp from 'hooks/useHttp';
-import { useModal } from 'hooks/useStoreContext';
-import errorQuery from 'utils/errorQuery';
-import { randIcons } from 'utils/categoryIcons';
+import useCreateCategory from 'hooks/useCreateCategory';
 
 const categoryLong = 100;
 
 const CategoryForm = () => {
-  const queryClient = useQueryClient();
-  const { requestHttp } = useHttp();
-  const [categoryName, setCategoryName] = useState('');
-
-  const { isModalShow, setShowModal } = useModal();
-
-  const { mutate: mutateCategory } = useMutation({
-    mutationFn: (newCategory) => {
-      return requestHttp({
-        method: 'POST',
-        url: '/categories',
-        data: newCategory,
-      });
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: ['categories'] });
-      toast.success(data?.data.message);
-    },
-    onError: (error) => {
-      errorQuery(error, 'Add New Category Failed!');
-    },
-  });
-
-  let isInputEmpty = false;
-
-  if (categoryName) {
-    isInputEmpty = true;
-  }
-
-  const categoryNameChangeHandler = (event) => {
-    setCategoryName((prevState) => {
-      return event.target.value.length <= categoryLong
-        ? event.target.value
-        : prevState;
-    });
-  };
-
-  const categoryCancelHandler = () => {
-    setShowModal(false);
-    setCategoryName('');
-  };
-
-  const categorySubmitHandler = (event) => {
-    event.preventDefault();
-
-    const newCategory = {
-      name: categoryName,
-      icon: randIcons,
-    };
-
-    mutateCategory(newCategory);
-
-    setShowModal(false);
-    setCategoryName('');
-  };
+  const {
+    isInputEmpty,
+    categoryName,
+    categorySubmitHandler,
+    categoryNameChangeHandler,
+    categoryCancelHandler,
+  } = useCreateCategory();
 
   return (
     <>
-      {isModalShow && (
-        <Modal onCloseModalHandler={() => setShowModal(false)}>
-          <h1 className="mb-4 font-bold">Create Category</h1>
-          <form
-            onSubmit={categorySubmitHandler}
-            className="flex flex-col gap-y-4"
+      <form onSubmit={categorySubmitHandler} className="flex flex-col gap-y-4">
+        <div className="flex flex-row items-center justify-between dark:text-material-green">
+          <label
+            htmlFor="category-name"
+            className="text-sm after:ml-1 after:text-red-500 after:content-['*']"
           >
-            <div className="flex flex-row items-center justify-between">
-              <label
-                htmlFor="category-name"
-                className="text-sm after:ml-1 after:text-red-500 after:content-['*']"
-              >
-                Category Name
-              </label>
-              <span className="text-xs font-semibold">
-                {0 + categoryName.length}/{categoryLong}
-              </span>
-            </div>
-            <input
-              type="text"
-              name="category-name"
-              id="category-name"
-              onChange={categoryNameChangeHandler}
-              value={categoryName}
-              placeholder="Write your category name..."
-              className="rounded bg-neutral-200 p-2 outline-none placeholder:text-sm"
-            />
+            Category Name
+          </label>
+          <span className="text-xs font-semibold">
+            {0 + categoryName.length}/{categoryLong}
+          </span>
+        </div>
+        <input
+          type="text"
+          name="category-name"
+          id="category-name"
+          onChange={categoryNameChangeHandler}
+          value={categoryName}
+          placeholder="Write your category name..."
+          className="rounded bg-neutral-200 p-2 outline-none placeholder:text-sm dark:bg-neutral-800 dark:text-material-green"
+        />
 
-            <button
-              className="block cursor-pointer rounded bg-orange-100 p-2 font-semibold text-white disabled:cursor-not-allowed disabled:bg-orange-50"
-              disabled={!isInputEmpty}
-            >
-              Create Category
-            </button>
+        <button
+          className="block cursor-pointer rounded bg-orange-100 p-2 font-semibold text-material-green disabled:cursor-not-allowed disabled:bg-orange-50 dark:text-neutral-800"
+          disabled={!isInputEmpty}
+        >
+          Create Category
+        </button>
 
-            <button
-              type="button"
-              onClick={categoryCancelHandler}
-              className="block cursor-pointer rounded p-2 font-semibold text-orange-100 disabled:bg-orange-50"
-            >
-              Cancel
-            </button>
-          </form>
-        </Modal>
-      )}
+        <button
+          type="button"
+          onClick={categoryCancelHandler}
+          className="block cursor-pointer rounded p-2 font-semibold text-orange-100 disabled:bg-orange-50"
+        >
+          Cancel
+        </button>
+      </form>
     </>
   );
 };
--- src/pages/Category.page.jsx
+++ src/pages/Category.page.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { Toaster } from 'react-hot-toast';
 
-import CategoryForm from 'components/Category/CategoryForm';
+import CategoryFormModal from 'components/Category/CategoryFormModal';
 import CategoryItem from 'components/Category/CategoryItem';
 import Pagination from 'components/UI/Pagination';
 import Search from 'components/UI/Search';
@@ -13,11 +13,10 @@ import useBaffle from 'hooks/useBaffle';
 import { useModal } from 'hooks/useStoreContext';
 import errorQuery from 'utils/errorQuery';
 
-import { MdArrowBack } from 'react-icons/md';
-import { ReactComponent as Plus } from 'assets/icons/uil_plus.svg';
+import { MdArrowBack, MdAdd } from 'react-icons/md';
+import emptyCategory from 'assets/images/categories.webp';
 
 const Category = () => {
-  const [showCategoryForm, setShowCategoryForm] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [skipCategoryPaginate, setSkipCategoryPaginate] = useState(0);
   const [pageSize, setPageSize] = useState(5);
@@ -43,6 +42,7 @@ const Category = () => {
     data: categories,
     isError: isErrorCategories,
     isLoading: isLoadingCategories,
+    isFetching: isFetchingCategories,
     error: errorCategories,
     refetch,
   } = useQuery({
@@ -64,19 +64,21 @@ const Category = () => {
     },
   });
 
-  const allCategories = categories?.data.data.rows;
+  const allCategoriesData = categories?.data.data.rows;
   const categoriesCount = +categories?.data.data.count;
 
   useEffect(() => {
     window.scrollTo({ behavior: 'smooth', top: 0 });
-  }, [allCategories]);
+  }, [allCategoriesData]);
 
   const showCategoryFormHandler = () => {
     setShowModal((prevState) => !prevState);
   };
+
   const searchValueHandler = (data) => {
     setSearchCategories(data);
   };
+
   const onPageChangeHandler = (page) => {
     setCurrentPage(page);
   };
@@ -85,52 +87,85 @@ const Category = () => {
     setSkipCategoryPaginate(pages);
   };
 
+  const backButtonHandler = () => {
+    refetch();
+    setIsButtonShow(false);
+    setSearchCategories('');
+  };
+
   const categoriesContent = (
     <>
-      <ul className="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
-        <button
-          type="button"
-          onClick={showCategoryFormHandler}
-          className="flex min-h-[6rem] w-full cursor-pointer flex-col items-center justify-center gap-y-1 rounded border-2 border-dashed border-orange-100 py-4 text-sm font-semibold text-orange-100"
-        >
-          <Plus fill="#FF844B" className="h-5 w-5" /> Add Category
-        </button>
-        {allCategories?.map((category) => {
-          return <CategoryItem {...category} key={category.id} />;
-        })}
-      </ul>
-      {categoriesCount > pageSize && (
-        <Pagination
-          currentPage={currentPage}
-          totalCount={categoriesCount}
-          pageSize={pageSize}
-          onPageChange={onPageChangeHandler}
-          onSkipPage={onSetSkipChangeHandler}
-          onSetPageSize={setPageSize}
-        />
+      {categoriesCount === 0 ? (
+        <div className="mx-auto flex min-h-[50vh] flex-col items-center justify-center gap-y-3">
+          <img
+            src={emptyCategory}
+            alt=""
+            className="max-w-[4rem] md:max-w-[5rem]"
+            loading="lazy"
+          />
+          <p className="text-center text-lg font-medium dark:text-material-green">
+            Category Empty
+          </p>
+          <button
+            type="button"
+            onClick={showCategoryFormHandler}
+            className="flex items-center gap-x-1 rounded-md bg-orange-100 py-2 px-4 font-semibold text-material-green shadow-material-shadow dark:text-neutral-800"
+          >
+            <MdAdd className="text-2xl text-material-green dark:text-neutral-800" />{' '}
+            Add Category
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="min-h-screen">
+            <ul className="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+              <button
+                type="button"
+                onClick={showCategoryFormHandler}
+                className="flex min-h-[8rem] w-full cursor-pointer flex-col items-center justify-center gap-y-1 rounded border-2 border-dashed border-orange-100 py-4 text-sm font-semibold text-orange-100"
+              >
+                <MdAdd className="text-2xl text-orange-100" /> Add Category
+              </button>
+              {allCategoriesData?.map((category) => {
+                return <CategoryItem {...category} key={category.id} />;
+              })}
+            </ul>
+          </div>
+          <Pagination
+            currentPage={currentPage}
+            totalCount={categoriesCount}
+            pageSize={pageSize}
+            onPageChange={onPageChangeHandler}
+            onSkipPage={onSetSkipChangeHandler}
+            onSetPageSize={setPageSize}
+          />
+        </>
       )}
     </>
   );
 
   return (
     <>
-      <Toaster position="top-center" />
-      <section className="flex min-h-screen flex-col gap-y-6 py-6">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 1500,
+        }}
+      />
+      <section className="layout flex min-h-screen flex-col gap-y-8 py-6 pt-20">
         <div className="flex flex-col items-center justify-center gap-y-5 sm:flex-row sm:justify-between">
           <div className="flex items-center gap-x-6">
             {isButtonShow && (
               <button
-                className="rounded bg-white py-1 px-3 shadow-material-shadow duration-300 hover:ring-2 hover:ring-orange-100"
-                onClick={() => {
-                  refetch();
-                  setIsButtonShow(false);
-                  setSearchCategories('');
-                }}
+                className="rounded bg-material-green py-1 px-3 shadow-material-shadow duration-300 hover:ring-2 hover:ring-orange-100 dark:bg-orange-100"
+                onClick={backButtonHandler}
               >
                 <MdArrowBack className="text-xl" />
               </button>
             )}
-            <h1 className="categoryBaffle text-lg font-bold">Category</h1>
+            <h1 className="categoryBaffle text-lg font-bold dark:text-material-green">
+              Category
+            </h1>
           </div>
           <Search name="Categories" onSearchValue={searchValueHandler} />
         </div>
@@ -148,15 +183,12 @@ const Category = () => {
               errorCategories?.message}
           </p>
         )}
-        {isLoadingCategories && (
+        {isLoadingCategories && isFetchingCategories && (
           <p className="text-center text-lg font-semibold">Loading...</p>
         )}
         {!isErrorCategories && !isLoadingCategories && categoriesContent}
       </section>
-      <CategoryForm
-        onShowCategoryForm={showCategoryForm}
-        onSetShowCategoryForm={setShowCategoryForm}
-      />
+      <CategoryFormModal />
     </>
   );
 };
